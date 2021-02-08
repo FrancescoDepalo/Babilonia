3d36c0d5d288cde35f36efb621516eb9b004d962
Francesco Depalo <francescodepalo@github.com>
2021-02-08 17:26:04 +0100

    game

---
+++ Assets/Joystick.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5.0f;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    public Transform circle;
+    public Transform outerCircle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            circle.transform.position = pointB;
+            outerCircle.transform.position = pointB;
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else 
+        {
+            touchStart = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharapter(direction * -1);
+            circle.transform.position = new Vector2(pointB.x + direction.x * -1, pointB.y + direction.y * -1);
+        }
+        else 
+        {
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    void moveCharapter(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
